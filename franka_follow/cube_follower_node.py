dcc2e0df38a21e302698029a9726000eba6a1b16
RafalS3 <rafalsawczyszyn@gmail.com>
2026-02-23 14:33:46 +0000

    launch file for an integration with MoveIt!

--- franka_follow/cube_follower_node.py
+++ franka_follow/cube_follower_node.py
@@ -1,23 +1,42 @@
+import math
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 from geometry_msgs.msg import TwistStamped
 from tf2_ros import Buffer, TransformListener
 from tf2_ros import TransformException
-
+from trajectory_msgs.msg import JointTrajectory
+from sensor_msgs.msg import JointState
 class CubeFollower(Node):
     def __init__(self):
-        super().__init__('cube_follower_node')
+        super().__init__(
+            'cube_follower_node',
+            parameter_overrides=[Parameter('use_sim_time', Parameter.Type.BOOL, True)]
+        )
         
         self.publisher = self.create_publisher(
             TwistStamped, 
             '/servo_node/delta_twist_cmds', 
             10
         )
+        self.traj_sub = self.create_subscription(JointTrajectory, '/panda_arm_controller/joint_trajectory', self.traj_cb, 10)
+        self.js_pub = self.create_publisher(JointState, '/joint_commands', 10)
         
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         
         self.timer = self.create_timer(0.05, self.control_loop)
+
+    def traj_cb(self, msg: JointTrajectory):
+        if not msg.points:
+            return
+        js = JointState()
+        js.header = msg.header
+        js.header.stamp = self.get_clock().now().to_msg()
+        js.name = msg.joint_names
+        js.position = msg.points[0].positions
+        js.velocity = msg.points[0].velocities
+        self.js_pub.publish(js)
 
     def control_loop(self):
         try:
@@ -35,15 +54,21 @@ class CubeFollower(Node):
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = 'panda_hand'
 
-        gain = 1.5
-        
-        msg.twist.linear.x = trans.transform.translation.x * gain
-        msg.twist.linear.y = trans.transform.translation.y * gain
-        msg.twist.linear.z = trans.transform.translation.z * gain
-        
-        msg.twist.angular.x = 0.0
-        msg.twist.angular.y = 0.0
-        msg.twist.angular.z = 0.0
+        dx = trans.transform.translation.x
+        dy = trans.transform.translation.y
+        dz = trans.transform.translation.z
+
+        distance = math.sqrt(dx**2 + dy**2 + dz**2)
+
+        gain = 1.0  
+        if distance < 0.10:
+            msg.twist.linear.x = 0.0
+            msg.twist.linear.y = 0.0
+            msg.twist.linear.z = 0.0
+        else:
+            msg.twist.linear.x = dx * gain
+            msg.twist.linear.y = dy * gain
+            msg.twist.linear.z = dz * gain
 
         self.publisher.publish(msg)
 
---
+++ launch/franka.launch.py
@@ -0,0 +1,96 @@
+import os
+import yaml  
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_path
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch.actions import (
+    OpaqueFunction,
+    ExecuteProcess,
+    TimerAction
+)
+
+
+def load_yaml(file_path):
+    with open(file_path, "r") as file:
+        return yaml.safe_load(file)
+
+
+moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_moveit_configs()
+rviz_config_file = os.path.join(
+    get_package_share_path("franka_follow"), "config", "franka.rviz"
+)
+
+servo_yaml_path = os.path.join(
+    get_package_share_path("moveit_servo"), "config", "panda_simulated_config.yaml"
+)
+
+servo_params = load_yaml(servo_yaml_path)
+
+
+def launch_setup(context):
+    actions = []
+
+    actions += [
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            arguments=["-d", rviz_config_file],
+            parameters=[
+                moveit_config.robot_description,
+                moveit_config.robot_description_semantic,
+                moveit_config.robot_description_kinematics,
+            ],
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="both",
+            parameters=[moveit_config.robot_description, {"use_sim_time": True}],
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="virtual_joint_broadcaster",
+            arguments=["0", "0", "0", "0", "0", "0", "world", "panda_link0"],
+            output="screen",
+        ),
+        Node(
+            package="moveit_servo",
+            executable="servo_node_main",
+            name="servo_node",
+            parameters=[
+                servo_params,
+                moveit_config.robot_description,
+                moveit_config.robot_description_semantic,
+                moveit_config.robot_description_kinematics,
+                {"use_sim_time": True},
+            ],
+            output="screen",
+        ),
+        Node(
+            package="franka_follow",
+            executable="cube_follower",
+        ),
+        # Moveit needs to be triggered 
+        TimerAction(
+        period=4.0,
+        actions=[
+            ExecuteProcess(
+                cmd=[
+                    "ros2", "service", "call",
+                    "/servo_node/start_servo",
+                    "std_srvs/srv/Trigger",
+                    "{}"
+                ],
+                output="screen"
+            )
+        ]
+    )
+    ]
+
+    return actions
+
+
+def generate_launch_description():
+    return LaunchDescription([OpaqueFunction(function=launch_setup)])
